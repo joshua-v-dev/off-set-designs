a4f9a33e5682174b9b2b9f49f197089fad3483df
joshua-v-dev <joshua-v-dev@gmail.com>
2021-11-01 11:51:43 -0600

    gitignore, authmiddleware.js, token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const protect = asyncHandler(async (req, res, next) => {
 
 	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
 		try {
-			token = req.headers.authorization.split(' ')[1]
+			let token = req.headers.authorization.split(' ')[1]
 
 			const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,12 +1,3 @@
-// import jwt from 'jsonwebtoken';
-//
-// const generateToken = (id) => {
-//   return jwt.sign({id}, process.env.JWT_SECRET, {
-//     expiresIn: '30d',
-//   });
-// };
-//
-// export default generateToken;
 import jwt from 'jsonwebtoken'
 
 const generateToken = (id) => {
